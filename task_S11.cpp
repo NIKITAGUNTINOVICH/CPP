b2d0749e76b3c28458f5271bbccfee439a8997f0
NIKITAGUNTINOVICH <guntinovich.n@gmail.com>
2025-12-07 22:51:33 +0800

    Create task_S11.cpp

---
+++ task_S11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i <= 100; ++i) {
+        if (i % 3 == 0) {
+            std::cout << i << " ";
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
